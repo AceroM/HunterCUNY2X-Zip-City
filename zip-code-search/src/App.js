ab3d0b4a6c9d7e5e3e3820a3ef63bccee33067eb
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-01-09 11:45:23 -0500

    switched names so it's correct now

--- zip-code-search/src/App.js
+++ zip-code-search/src/App.js
@@ -2,24 +2,88 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Card = props => {
+  return (
+      <div className="card-container">
+          <h2> {props.name}</h2>
+          <ul>
+              <li>State: {props.place}</li>
+              <li>Location: {props.location}</li>
+              <li>Population (estimated): {props.population}</li>
+              <li>Total Wages: {props.wages} </li>
+          </ul>
+      </div>
+  )
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardArr: [],
+      zip: "",
+      isError: false
+    }
+  }
+
+  handleEnter = (e) => {
+    if (e.keyCode == 13) {
+      fetch(`http://ctp-zip-api.herokuapp.com/zip/${this.state.zip}`)
+      .then(res => res.json())
+      .then(zip => {
+        console.log(zip)
+        let newState = zip.map(item => {
+          return (
+            {
+              name: item.LocationText,
+              place: item.State,
+              location: `(${item.Lat}, ${item.Long})`,
+              population: item.EstimatedPopulation,
+              wages: item.TotalWages
+            }
+          )
+        })
+        this.setState({ cardArr: newState })
+      })
+      .catch((err) => {
+        console.error(err);
+        this.setState({ isError: true })
+      })
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({zip: e.target.value})
+  }
+
   render() {
+    const Cards = this.state.cardArr.map(values => (
+      <Card 
+        name={values.name}
+        place={values.place}
+        location={values.location}
+        population={values.population}
+        wages={values.wages}
+      />
+    ))
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1> Zip Code Search </h1>
         </header>
+        Zip Code:
+        <input type="text"
+          onKeyDown={this.handleEnter}
+          onChange={this.handleChange} 
+          value={this.state.zip} 
+          id="search"
+        />
+        { this.state.isError ?
+          <p> No Results </p>
+          : ""
+        }
+      { Cards }
       </div>
     );
   }
