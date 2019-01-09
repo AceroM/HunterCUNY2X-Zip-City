535a517564fb68a62a6c4befb5ceeaf782f5cc95
shane <shanejlester@gmail.com>
2019-01-09 09:32:42 -0800

    Finish city search functionlity

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -2,24 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardArr: [],
+      city: "",
+      isError: false
+    }
+  }
+
+  handleEnter = (e) => {
+    if (e.keyCode == 13) {
+      fetch(`http://ctp-zip-api.herokuapp.com/city/${encodeURIComponent(this.state.city.toUpperCase())}`)
+      .then(res => res.json())
+      .then(zipCodes => {
+      console.log(zipCodes);
+          this.setState({
+                cardArr: zipCodes,
+                isError: false
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+        this.setState({ isError: true })
+      })
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({city: e.target.value})
+  }
+
   render() {
+    const zips = this.state.cardArr.map((zip) => (
+        <li>
+            {zip}
+        </li>
+    ));
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1> City Name Search </h1>
         </header>
+        City Name:
+        <input type="text"
+          onKeyDown={this.handleEnter}
+          onChange={this.handleChange} 
+          value={this.state.city} 
+          id="search"
+        />
+        { this.state.isError ?
+          <p> No Results </p>
+          : ""
+        }
+        <ul>
+            {this.state.isError ? "" : zips}
+        </ul>
       </div>
     );
   }
